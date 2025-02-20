283ea6bcfcf16e3115c63493446dbd1f3d42af39
sindhusb03 <sindhusb03@hotmail.com>
2025-02-20 12:01:20 +0530

    Feat: Implemented support for custom delimiters

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ class StringCalculator:
     def add(numbers: str) -> int:
         if numbers == "":
             return 0
-        num_list = list(map(int, re.split(",|\n", numbers)))
+
+        delimiter = ","
+        if numbers.startswith("//"):
+            delimiter, numbers = numbers[2:].split("\n", 1)
+
+        num_list = list(map(int, re.split(f"[{delimiter}\n]", numbers)))
         return sum(num_list)
+
 
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
@@ -20,6 +26,8 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(StringCalculator.add("1,2,3,4"), 10)
     def test_newline_as_delimiter(self):
         self.assertEqual(StringCalculator.add("1\n2,3"), 6)
+    def test_custom_delimiter(self):
+        self.assertEqual(StringCalculator.add("//;\n1;2"), 3)
 
 
 if __name__ == "__main__":
