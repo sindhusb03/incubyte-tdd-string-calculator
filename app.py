fcef32cf688575363a2f1ba626d878ff56909256
sindhusb03 <sindhusb03@hotmail.com>
2025-02-20 11:59:32 +0530

    Feat: Implemented add() function to sum multiple numbers

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ class StringCalculator:
     def add(numbers: str) -> int:
         if numbers == "":
             return 0
-        num_list = list(map(int, numbers.split(","))) 
+        num_list = list(map(int, numbers.split(",")))
         return sum(num_list)
-
 
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
@@ -16,6 +15,8 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(StringCalculator.add("1"), 1)
     def test_two_numbers(self):
         self.assertEqual(StringCalculator.add("1,5"), 6)
+    def test_multiple_numbers(self):
+        self.assertEqual(StringCalculator.add("1,2,3,4"), 10)
 
 if __name__ == "__main__":
     unittest.main()
