a3e2cb7c0e3685c22a189c6eb5492f970fbb8b66
sindhusb03 <sindhusb03@hotmail.com>
2025-02-20 11:56:24 +0530

    Test: Added test for single number input

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ class StringCalculator:
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(StringCalculator.add(""), 0)
+    def test_single_number(self):
+        self.assertEqual(StringCalculator.add("1"), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
